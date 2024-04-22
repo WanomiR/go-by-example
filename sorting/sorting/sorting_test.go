d13d36ff8c495fd81111a31fb24f0e094808c2ca
Wanomir <ioan.romadin@gmail.com>
2024-04-22 08:19:50 +0300

    feat: heap sort and benchmarks

--- sorting/sorting/sorting_test.go
+++ sorting/sorting/sorting_test.go
@@ -1,7 +1,9 @@
 package sorting
 
 import (
+	"math/rand"
 	"reflect"
+	"sync/atomic"
 	"testing"
 )
 
@@ -29,6 +31,17 @@ func TestBubbleSort(t *testing.T) {
 	}
 }
 
+func BenchmarkBubbleSort(b *testing.B) {
+	data := genData()
+	var count int64
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		BubbleSort(data)
+		atomic.AddInt64(&count, 1)
+	}
+}
+
 func TestSelectionSort(t *testing.T) {
 	testCases := []struct {
 		name string
@@ -50,6 +63,17 @@ func TestSelectionSort(t *testing.T) {
 				t.Errorf("Calling SelectionSort() = %v, want %v", result, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkSelectionSort(b *testing.B) {
+	data := genData()
+	var count int64
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		SelectionSort(data)
+		atomic.AddInt64(&count, 1)
 	}
 }
 
@@ -77,6 +101,17 @@ func TestInsertionSort(t *testing.T) {
 	}
 }
 
+func BenchmarkInsertionSort(b *testing.B) {
+	data := genData()
+	var count int64
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		InsertionSort(data)
+		atomic.AddInt64(&count, 1)
+	}
+}
+
 func TestQuickSort(t *testing.T) {
 	testCases := []struct {
 		name string
@@ -98,6 +133,17 @@ func TestQuickSort(t *testing.T) {
 				t.Errorf("Calling QuickSort() = %v, want %v", result, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkQuickSortSort(b *testing.B) {
+	data := genData()
+	var count int64
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		QuickSort(data, 0, len(data)-1)
+		atomic.AddInt64(&count, 1)
 	}
 }
 
@@ -125,6 +171,17 @@ func TestMergeSort(t *testing.T) {
 	}
 }
 
+func BenchmarkMergeSortSort(b *testing.B) {
+	data := genData()
+	var count int64
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		MergeSort(data)
+		atomic.AddInt64(&count, 1)
+	}
+}
+
 func TestCountSort(t *testing.T) {
 	testCases := []struct {
 		name string
@@ -147,4 +204,63 @@ func TestCountSort(t *testing.T) {
 			}
 		})
 	}
+}
+
+func BenchmarkCountSortSort(b *testing.B) {
+	data := genData()
+	var count int64
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CountSort(data)
+		atomic.AddInt64(&count, 1)
+	}
+}
+
+func TestHeapSort(t *testing.T) {
+	testCases := []struct {
+		name string
+		arr  []int
+		want []int
+	}{
+		{"empty array", []int{}, []int{}},
+		{"one value", []int{0}, []int{0}},
+		{"repeating value", []int{1, 1, 1}, []int{1, 1, 1}},
+		{"negative values", []int{-4, -1, -2, -3, -5}, []int{-5, -4, -3, -2, -1}},
+		{"positive values", []int{4, 1, 2, 3, 5}, []int{1, 2, 3, 4, 5}},
+		{"values from -10 to 10", []int{10, -1, -2, 4, 3, 8, 0, -5, -6, 5}, []int{-6, -5, -2, -1, 0, 3, 4, 5, 8, 10}},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			result := HeapSort(tt.arr)
+			if !reflect.DeepEqual(result, tt.want) {
+				t.Errorf("Calling HeapSort() = %v, want %v", result, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkHeapSortSort(b *testing.B) {
+	data := genData()
+	var count int64
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		HeapSort(data)
+		atomic.AddInt64(&count, 1)
+	}
+}
+
+func genData() []int {
+	rand.Seed(2_147_483_647)
+
+	n := 1_000
+
+	data := make([]int, 0, n)
+	for j := 0; j < n; j++ {
+		data = append(data, rand.Intn(1_001)-500)
+	}
+
+	return data
 }
